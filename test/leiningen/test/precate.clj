6ea2d6c1f67c68dd890853bdf7f3d34b6429c3ec
Nelson Morris <nmorris@nelsonmorris.net>
2012-04-03 21:51:43 -0500

    Keep dependency options and convert :type to :extension

--- test/leiningen/test/precate.clj
+++ test/leiningen/test/precate.clj
@@ -11,14 +11,19 @@
         p1 (tidy-project p1-full)]
     (is (= {:name "hi" :group "hello" :version "1.0.0"
             :dependencies [['ordered "1.1.0"]
-                           ['org.clojure/clojure "1.3.0"]]
+                           ['org.clojure/clojure "1.3.0"]
+                           ['something "0.0.1" :extension "pom" :classifier "sources"]]
             :min-lein-version "2.0.0"
-            :profiles {:dev {:dependencies [['robert/hooke "1.1.3"]]
+            :profiles {:dev {:dependencies [['robert/hooke "1.1.3"]
+                                            ['something-dev "0.0.1" :extension "pom"]]
                              :resource-paths ["junk"]}
-                       :1.2.1 {:dependencies [['org.clojure/clojure "1.2.1"]]}
+                       :1.2.1 {:dependencies [['org.clojure/clojure "1.2.1"]
+                                              ['something-early "0.0.1" :extension "pom"]]
+                               }
                        :1.4.0 {:dependencies [['org.clojure/clojure "1.4.0-beta1"]
                                               ['slingshot "0.10.1"]
-                                              ['cheshire "2.2.0"]]}}
+                                              ['cheshire "2.2.0"]
+                                              ['something-pre "0.0.1" :extension "pom"]]}}
             :plugins [['lein-swank "1.4.1"]
                       ['lein-tar "1.0.6"]]}
            (dissoc p1 :source-paths :resource-paths :root)))
